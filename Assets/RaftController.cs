664edac88bc5e3913c07dd6500360a24520e0c63
nliebers <nliebers24@gmail.com>
2022-03-18 11:04:52 -0500

    Added controllable raft.

---
+++ Assets/RaftController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaftController : MonoBehaviour
+{
+
+	private UnityEngine.AI.NavMeshAgent agent;
+	public GameObject player;
+	public GameObject mountPoint;
+	public GameObject sailDirection;
+
+	void OnEnable () {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) <= 2.0f ){
+			player.transform.position = mountPoint.transform.position;
+			Vector3 newPos = sailDirection.transform.forward * 1.0f;
+			Debug.Log(sailDirection.transform.forward);
+            agent.SetDestination(transform.position + newPos);
+		}
+    }
+	
+	private void FaceTarget(Vector3 destination)
+	{
+		Vector3 lookPos = agent.destination - transform.position;
+		lookPos.y = 0;
+		Quaternion rotation = Quaternion.LookRotation(lookPos);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.1f);  
+	}
+}
