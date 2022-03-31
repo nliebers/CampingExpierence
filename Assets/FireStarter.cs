c5dea6373566f73df9d13cea36a5b1f732d88b34
nliebers <nliebers24@gmail.com>
2022-03-31 15:37:15 -0500

    Fire

--- Assets/FireStarter.cs
+++ Assets/FireStarter.cs
@@ -9,16 +9,17 @@ namespace Valve.VR.InteractionSystem
     {
 
         public bool sparked = false;
-        public int sparkingVelocity = 0;
-        public int velocityInt = 0;
+        public int fireProximity = 10;
 
 
         private Vector3 prevPosition;
         private Quaternion prevRotation;
         private Vector3 prevVelocity;
         private Vector3 prevHeadPosition;
+        private int sparkingChance;
 
         public GameObject player;
+        public GameObject CampFire;
         public GameObject fireRing;
         public GameObject leftHand;
         public GameObject rightHand;
@@ -35,40 +36,33 @@ namespace Valve.VR.InteractionSystem
             }
         }
 
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.transform.gameObject.tag == "FireStick") {
+                sparkingChance = UnityEngine.Random.Range(1, 10);
+                if (sparkingChance == 2) {
+                    SparkFire();
+                }
+            }
+        }
+
         void FixedUpdate()
         {
             if (sparked)
             {
-                prevPosition = transform.position;
-                prevRotation = transform.rotation;
-                transform.GetComponent<Rigidbody>().velocity *= 0.99f;
-                prevVelocity = GetComponent<Rigidbody>().velocity;
-                prevHeadPosition = transform.position;
+
             }
         }
 
         public void SparkFire()
         {
             Debug.Log("sparked");
+            if (Vector3.Distance(fireRing.transform.position, player.transform.position) <= fireProximity) {
+                //set some game object active
+                CampFire.SetActive(true);
+            }
             sparked = true;
-            // Activate the sparking particles and have the fire start
-        }
-
-        private IEnumerator ThrowSpearAfterRelease(float wait)
-        {
-            Vector3 tipForward = tip.transform.forward;
-
-            yield return new WaitForSeconds(wait);
-            if (FloorToInt(transform.GetComponent<Rigidbody>().velocity, velocityInt) > sparkingVelocity)
-            {
-                // DO a sparking action here
-            }
-
-            if (leftHand != null || rightHand != null)
-            {
-                Vector3 greatestVelo = Vector3.Max(leftHand.GetComponent<HandPhysics>().handCollider.GetComponent<Rigidbody>().velocity, rightHand.GetComponent<HandPhysics>().handCollider.GetComponent<Rigidbody>().velocity);
-                transform.GetComponent<Rigidbody>().velocity = tipForward * greatestVelo.magnitude * 10f;
-            }
+            
         }
 
         private int FloorToInt(Vector3 velocity, object v)
