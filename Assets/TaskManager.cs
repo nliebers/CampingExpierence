47458d1d0bab768fc744bb136a963c5cfd947642
nliebers <nliebers24@gmail.com>
2022-04-05 14:34:51 -0500

    TaskManager no longer duplicates on scene re-load.

--- Assets/TaskManager.cs
+++ Assets/TaskManager.cs
@@ -9,9 +9,16 @@ public class TaskManager : MonoBehaviour
 	public bool water;
 	public bool fire;
 	public bool food;
+	private static TaskManager taskManagerInstance;
 	public int day = 1;
 	
     void Awake() {
 		DontDestroyOnLoad(this.gameObject);
+		if (taskManagerInstance == null) {
+			taskManagerInstance = this;
+		}
+		else {
+			Destroy(gameObject);
+		}
 	}
 }
